56d466561a6e7c50cbd6b865b5db1b5542c39b5a
Alexander Graham <alexinbristol@outlook.com>
2024-11-06 09:24:15 +0000

    Added comments on LoginView and furthered understanding in this area. Started to implement dropdown menu for login and logout.

--- recipe_world_project/accounts/views.py
+++ recipe_world_project/accounts/views.py
@@ -10,15 +10,15 @@ class SignUpView(CreateView):
     template_name = 'registration/signup.html'
 
 class LoginView(LoginView):
+    # Redirect user if successful login. Default is False.
     redirect_authenticated_user = True
 
     def get_success_url(self):
+        # The URL the user will be redirected to. 
         return reverse_lazy('home')
     
     def form_invalid(self, form):
+        # Form_invalid() is called once the login is failed.
+        # In the form_invalid(), we create a flash message and rerender the login form.
         messages.error(self.request, 'Invalid username or password')
         return self.render_to_response(self.get_context_data(form=form))
-
-
-
-# Create your views here.
